d61ce6ff33ff8dad4fb4ecf644d5232decb53e2c
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-04 19:57:39 -0400

    students without math/language arts

--- grow/student-services-lau/students-without-math-language-arts.sql
+++ grow/student-services-lau/students-without-math-language-arts.sql
@@ -4,14 +4,10 @@ CREATE PROCEDURE sp_test_math_la()
 
 BEGIN
 
-DROP TEMPORARY TABLE IF EXISTS `m_la`;
-CREATE TEMPORARY TABLE m_la (id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-                                      `pid` int, `mla` int);
+drop temporary table if exists mla_all;
+create temporary table mla_all 
 
-INSERT INTO m_la (`pid`, `mla`)
-
-
-SELECT distinct e.personid as 'pid',  IF(pr.programid IN(5,2,18,20,21,11), 1, 0) as 'mla'
+SELECT distinct e.personid as 'pid' 
     FROM person p 
     JOIN enrollment e USING (personid)
     JOIN `user` u ON u.userid=e.personid 
@@ -21,7 +17,7 @@ SELECT distinct e.personid as 'pid',  IF(pr.programid IN(5,2,18,20,21,11), 1, 0)
     JOIN `schedule` h ON h.sectionid=s.sectionid 
     JOIN person t ON t.personid=s.teacherid 
     JOIN program_class pc ON pc.classid=s.classid
-    JOIN program pr ON pr.programid=pc.programid  
+    JOIN program pr ON pr.programid=pc.programid --  and pr.programid IN(5,2,18,20,21,11)
     JOIN enrollmenttostudentdebit esd USING (enrollmentid)
     JOIN studentdebit sd USING (studentdebitid)
     LEFT JOIN thirdpartypayerengagement tppe USING (tppengagementid)
@@ -29,14 +25,35 @@ SELECT distinct e.personid as 'pid',  IF(pr.programid IN(5,2,18,20,21,11), 1, 0)
 
 WHERE s.semesterid = 44 AND e.credithours > 0 AND e.statusid = 1 AND h.begindate < DATE(NOW()) AND tpp.TPPayerID = 12612
 
-order by pid, mla desc;
-
-
+group by pid
+order by pid;
         
-   
-select pid, mla from m_la
-group by pid;
-        
+
+drop temporary table if exists mla_yes;
+create temporary table mla_yes
+SELECT distinct e.personid as 'pid' 
+    FROM person p 
+    JOIN enrollment e USING (personid)
+    JOIN `user` u ON u.userid=e.personid 
+    JOIN section s USING (sectionid)
+    JOIN class c ON c.classid=s.classid
+    JOIN semester m USING (semesterid)
+    JOIN `schedule` h ON h.sectionid=s.sectionid 
+    JOIN person t ON t.personid=s.teacherid 
+    JOIN program_class pc ON pc.classid=s.classid
+    JOIN program pr ON pr.programid=pc.programid and pr.programid IN(5,2,18,20,21,11)
+    JOIN enrollmenttostudentdebit esd USING (enrollmentid)
+    JOIN studentdebit sd USING (studentdebitid)
+    LEFT JOIN thirdpartypayerengagement tppe USING (tppengagementid)
+    LEFT JOIN thirdpartypayer tpp ON tpp.TPPayerID=tppe.TPPayerID
+
+WHERE s.semesterid = 44 AND e.credithours > 0 AND e.statusid = 1 AND h.begindate < DATE(NOW()) AND tpp.TPPayerID = 12612
+
+group by pid
+order by pid;
+
+    
+select pid from mla_all where pid not in(select pid from mla_yes);
 end $$
 DELIMITER ;
 

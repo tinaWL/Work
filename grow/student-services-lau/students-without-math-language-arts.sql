1a7e52707549bc1585c3f81def1db1b084e328a7
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-04 21:22:01 -0400

    cleaned up code

--- grow/student-services-lau/students-without-math-language-arts.sql
+++ grow/student-services-lau/students-without-math-language-arts.sql
@@ -1,4 +1,4 @@
-DELIMITER $$
+SELECT * FROM `e` WHERE 1DELIMITER $$
 DROP PROCEDURE IF EXISTS sp_test_math_la$$
 CREATE PROCEDURE sp_test_math_la()
 
@@ -7,7 +7,7 @@ BEGIN
 drop temporary table if exists mla_all;
 create temporary table mla_all 
 
-SELECT distinct e.personid as 'pid' 
+SELECT distinct e.personid as 'pid', pr.programid as 'prid'
     FROM person p 
     JOIN enrollment e USING (personid)
     JOIN `user` u ON u.userid=e.personid 
@@ -24,36 +24,19 @@ SELECT distinct e.personid as 'pid'
     LEFT JOIN thirdpartypayer tpp ON tpp.TPPayerID=tppe.TPPayerID
 
 WHERE s.semesterid = 44 AND e.credithours > 0 AND e.statusid = 1 AND h.begindate < DATE(NOW()) AND tpp.TPPayerID = 12612
-
-group by pid
 order by pid;
         
 
 drop temporary table if exists mla_yes;
 create temporary table mla_yes
-SELECT distinct e.personid as 'pid' 
-    FROM person p 
-    JOIN enrollment e USING (personid)
-    JOIN `user` u ON u.userid=e.personid 
-    JOIN section s USING (sectionid)
-    JOIN class c ON c.classid=s.classid
-    JOIN semester m USING (semesterid)
-    JOIN `schedule` h ON h.sectionid=s.sectionid 
-    JOIN person t ON t.personid=s.teacherid 
-    JOIN program_class pc ON pc.classid=s.classid
-    JOIN program pr ON pr.programid=pc.programid and pr.programid IN(5,2,18,20,21,11)
-    JOIN enrollmenttostudentdebit esd USING (enrollmentid)
-    JOIN studentdebit sd USING (studentdebitid)
-    LEFT JOIN thirdpartypayerengagement tppe USING (tppengagementid)
-    LEFT JOIN thirdpartypayer tpp ON tpp.TPPayerID=tppe.TPPayerID
-
-WHERE s.semesterid = 44 AND e.credithours > 0 AND e.statusid = 1 AND h.begindate < DATE(NOW()) AND tpp.TPPayerID = 12612
-
-group by pid
+SELECT distinct ma.pid as 'pid', ma.prid
+    FROM mla_all ma 
+    where ma.prid in(2,5,18,20)
 order by pid;
 
     
-select pid from mla_all where pid not in(select pid from mla_yes);
+select * from mla_all where pid not in (select pid from mla_yes)
+group by pid;
 end $$
 DELIMITER ;
 

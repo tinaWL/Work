7747a1f35421b1fe5757e05d6ac053ee6423e79f
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-05 00:31:25 -0400

    could this be the final list??

--- grow/student-services-lau/students-without-math-language-arts.sql
+++ grow/student-services-lau/students-without-math-language-arts.sql
@@ -7,7 +7,7 @@ BEGIN
 drop temporary table if exists mla_all;
 create temporary table mla_all 
 
-SELECT distinct e.personid as 'pid', pr.programid as 'prid', cf.filecount as 'LP'
+SELECT distinct  pr.programid as 'prid', e.personid as 'pid', cf.filecount as 'LP'
     FROM person p 
     JOIN enrollment e USING (personid)
     JOIN `user` u ON u.userid=e.personid 
@@ -34,14 +34,80 @@ order by pid;
 
 drop temporary table if exists mla_both;
 create temporary table mla_both
-SELECT distinct ma.pid as 'pid', ma.prid
+SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
     FROM mla_all ma 
     where ma.prid in(2,5,18,20)
 order by pid;
 
+
+
+drop temporary table if exists mla_no_math;
+create temporary table mla_no_math
+SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
+    FROM mla_all ma 
+    where ma.pid not in(select pid from mla_both where prid in(2,18))
+order by pid;
+
+drop temporary table if exists mla_no_la;
+create temporary table mla_no_la
+SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
+    FROM mla_all ma 
+    where ma.pid not in(select pid from mla_both where prid in(5,20))
+order by pid;
+
+drop temporary table if exists mla_neither;
+create temporary table mla_neither
+SELECT distinct ma.pid as 'pid'
+    FROM mla_all ma 
+    where ma.pid not in(select pid from mla_both) 
+    order by pid;
     
-select * from mla_all where pid not in (select pid from mla_both)
-group by pid;
+
+drop temporary table if exists math_final;
+create temporary table math_final
+SELECT distinct  pid
+    FROM mla_no_math 
+    where pid not in(select pid from mla_neither) 
+    order by pid;
+    
+    
+drop temporary table if exists la_final;
+create temporary table la_final
+SELECT distinct  pid
+    FROM mla_no_la 
+    where pid not in(select pid from mla_neither) 
+    order by pid;
+
+
+select * from la_final
+group by la_final.pid
+
+UNION
+
+select * from math_final
+group by math_final.pid
+
+UNION
+
+select * from mla_neither
+group by mla_neither.pid;
+/*drop temporary table if exists mla_none;
+create temporary table mla_none
+SELECT distinct ma.pid as 'pid', ma.prid as 'prid'
+    FROM mla_neither ma
+    where ma.pid not in(select pid from mla_no_math) 
+    order by pid;*/
+
+    
+-- select * from mla_all where pid not in (select pid from mla_both) -- don't have either 
+-- select * from mla_all where pid not in(select pid from mla_both where prid in(2,18)) -- don't have math
+-- select * from mla_all where pid not in(select pid from mla_both where prid in(5,20)) -- don't have language arts
+/*select ma.pid, la.pid, nm.pid, n.pid from mla_all ma
+left join mla_no_la la on la.pid = ma.pid
+left join mla_no_math nm on nm.pid = la.pid
+left join mla_neither n on n.pid = nm.pid
+group by ma.pid;*/
+-- left join mla_no_math nm on nm.pid = la.pid
 end $$
 DELIMITER ;
 

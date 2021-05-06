b02dfc0d4932aeab5664bf42e8556f950daef500
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-06 14:51:30 -0400

    update logan template id

--- ilc/ilc-registration-email.sql
+++ ilc/ilc-registration-email.sql
@@ -3,7 +3,7 @@
 SELECT e.personid, p.lastname, p.firstname, 
 
 (CASE 
-WHEN l.locationname = 'Logan ILC' THEN 1111
+WHEN l.locationname = 'Logan ILC' THEN 13304
 WHEN l.locationname = 'Ogden ILC' THEN 2222
 WHEN l.locationname = 'Pleasant Grove ILC' THEN 13302
 
@@ -15,7 +15,7 @@ JOIN `section` s USING (sectionid)
 JOIN `grade` g USING (gradeid)
 JOIN `class` c ON c.classid=s.classid
 JOIN location l using (locationid)
-LEFT JOIN externalactivity ea ON ea.RecipientPersonID=p.personid AND ea.ExternalID IN(1111,2222,13302)
+LEFT JOIN externalactivity ea ON ea.RecipientPersonID=p.personid AND ea.ExternalID IN(13304,2222,13302)
 WHERE ea.ExternalActivityID IS NULL AND s.semesterid>=46 AND e.statusid =1 AND e.credithours > 0 and l.locationid in (1,2,3,4,5) and s.divisionid = 4 AND s.classid NOT IN(703, 704, 710, 711)  
 group by e.personid;
 
@@ -26,7 +26,7 @@ group by e.personid;
 SELECT pg.personid, pg.lastname, pg.firstname, 
 
 (CASE 
-WHEN l.locationname = 'Logan ILC' THEN 1111
+WHEN l.locationname = 'Logan ILC' THEN 13304
 WHEN l.locationname = 'Ogden ILC' THEN 2222
 WHEN l.locationname = 'Pleasant Grove ILC' THEN 13302
 
@@ -40,6 +40,6 @@ JOIN `class` c ON c.classid=s.classid
 JOIN location l using (locationid)
 LEFT JOIN `person_relation` pr ON pr.secondpersonid=p.personid AND pr.deleted IS NULL 
 LEFT JOIN `person` pg ON pg.personid=pr.firstpersonid
-LEFT JOIN externalactivity ea ON ea.RecipientPersonID=p.personid AND ea.ExternalID IN(1111,2222,13302)
+LEFT JOIN externalactivity ea ON ea.RecipientPersonID=p.personid AND ea.ExternalID IN(13304,2222,13302)
 WHERE ea.ExternalActivityID IS NULL AND s.semesterid>=46 AND e.statusid =1 AND e.credithours > 0 and l.locationid in (1,2,3,4,5) and s.divisionid = 4 AND s.classid NOT IN(703, 704, 710, 711)  
 group by pg.personid;

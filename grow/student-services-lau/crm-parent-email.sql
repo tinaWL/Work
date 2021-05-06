10f076eecf1339ba97192920b26bae19019c9f65
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-06 18:37:43 -0400

    refactor

--- grow/student-services-lau/crm-parent-email.sql
+++ grow/student-services-lau/crm-parent-email.sql
@@ -4,10 +4,10 @@ CREATE PROCEDURE sp_test_math_la()
 
 BEGIN
 
-drop temporary table if exists mla_all;
-create temporary table mla_all 
+DROP TEMPORARY TABLE IF EXISTS mla_all;
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_all 
 
-SELECT distinct  e.programid as 'prid', e.personid as 'pid'
+SELECT DISTINCT  e.programid AS 'prid', e.personid AS 'pid'
     FROM enrollment e 
     JOIN section s USING (sectionid)
     JOIN enrollmenttostudentdebit esd USING (enrollmentid)
@@ -19,78 +19,80 @@ WHERE s.semesterid >= 46 AND e.credithours > 0 AND e.statusid = 1  AND tpp.TPPay
 order by pid;
         
 
-drop temporary table if exists mla_both; -- all students currently enrolled in math and/or language arts classes
-create temporary table mla_both
-SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
+DROP TEMPORARY TABLE IF EXISTS mla_both; -- all students currently enrolled in math and/or language arts classes
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_both
+SELECT DISTINCT  ma.prid AS 'prid', ma.pid AS 'pid'
     FROM mla_all ma 
-    where ma.prid in(2,5,18,20)
+    WHERE ma.prid in(2,5,18,20)
+ORDER BY pid;
+
+
+DROP TEMPORARY TABLE IF EXISTS mla_no_math; -- students not enrolled in any math class
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_no_math
+SELECT distinct  ma.prid AS 'prid', ma.pid AS 'pid'
+    FROM mla_all ma 
+    WHERE ma.pid NOT IN(SELECT pid FROM mla_both WHERE prid IN(2,18))
+ORDER BY pid;
+
+DROP TEMPORARY TABLE IF EXISTS mla_no_la; -- students not enrolled in any language arts class
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_no_la
+SELECT distinct  ma.prid AS 'prid', ma.pid AS 'pid'
+    FROM mla_all ma 
+    WHERE ma.pid NOT IN(SELECT pid FROM mla_both WHERE prid IN(5,20))
 order by pid;
 
-
-drop temporary table if exists mla_no_math; -- students not enrolled in any math class
-create temporary table mla_no_math
-SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
+DROP TEMPORARY TABLE IF EXISTS mla_neither; -- students enrolled in NEITHER math nor language arts 
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_neither
+SELECT DISTINCT ma.pid AS 'pid'
     FROM mla_all ma 
-    where ma.pid not in(select pid from mla_both where prid in(2,18))
-order by pid;
-
-drop temporary table if exists mla_no_la; -- students not enrolled in any language arts class
-create temporary table mla_no_la
-SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
-    FROM mla_all ma 
-    where ma.pid not in(select pid from mla_both where prid in(5,20))
-order by pid;
-
-drop temporary table if exists mla_neither; -- students enrolled in NEITHER math nor language arts 
-create temporary table mla_neither
-SELECT distinct ma.pid as 'pid'
-    FROM mla_all ma 
-    where ma.pid not in(select pid from mla_both) 
-    order by pid;
+    WHERE ma.pid NOT IN(SELECT pid FROM mla_both) 
+ORDER BY pid;
     
 
-drop temporary table if exists math_final; -- students ONLY missing math
-create temporary table math_final
-SELECT distinct  pid
+DROP TEMPORARY TABLE IF EXISTS math_final; -- students ONLY missing math
+CREATE TEMPORARY TABLE IF NOT EXISTS math_final
+SELECT DISTINCT pid
     FROM mla_no_math 
-    where pid not in(select pid from mla_neither) 
+    WHERE pid NOT IN(SELECT pid FROM mla_neither) 
     order by pid;
     
     
-drop temporary table if exists la_final; -- students ONLY missing language arts
-create temporary table la_final
-SELECT distinct  pid
+DROP TEMPORARY TABLE IF EXISTS la_final; -- students ONLY missing language arts
+CREATE TEMPORARY TABLE IF NOT EXISTS la_final
+SELECT DISTINCT  pid
     FROM mla_no_la 
-    where pid not in(select pid from mla_neither) 
-    order by pid;
+    WHERE pid NOT IN(SELECT pid FROM mla_neither) 
+ORDER BY pid;
 
 
-drop temporary table if exists final; -- combination of students ONLY missing math, ONLY missing language arts, and missing NEITHER (no overlap)
-create temporary table final
-select *, 'Language Arts' as 'Missing' from la_final
-group by la_final.pid
+DROP TEMPORARY TABLE IF EXISTS final; -- combination of students ONLY missing math, ONLY missing language arts, and missing NEITHER (no overlap)
+CREATE TEMPORARY TABLE IF NOT EXISTS final
+SELECT *, 'Language Arts' AS 'Missing' FROM la_final
+GROUP BY la_final.pid
 
 UNION
 
-select *, 'Math' as 'Missing' from math_final
+SELECT *, 'Math' AS 'Missing' FROM math_final
 group by math_final.pid
 
 UNION
 
-select *, 'Math or Language Arts' as 'Missing' from mla_neither 
-group by mla_neither.pid;
+SELECT *, 'Math or Language Arts' AS 'Missing' FROM mla_neither 
+GROUP BY mla_neither.pid;
 
 	
 
--- INSERT INTO externalactivity(`RecipientPersonID`, `ActivityType`, `ExternalID`, `CustomField01`, `CreatedDate`)
+INSERT INTO externalactivity(`RecipientPersonID`, `ActivityType`, `ExternalID`, `CustomField01`, `CustomField02`,`CreatedDate`)
 SELECT DISTINCT pg.personid, 'InfusionSoft_Email', 13312, p.firstname, f.missing, NOW()
 FROM final f
 JOIN person p on p.personid = f.pid
 LEFT JOIN `person_relation` pr ON pr.secondpersonid=p.personid AND pr.deleted IS NULL 
 LEFT JOIN `person` pg ON pg.personid=pr.firstpersonid
+LEFT JOIN externalactivity ea ON ea.RecipientPersonID=pg.personid AND ea.ExternalID = 13312
+WHERE ea.ExternalActivityID IS NULL 
 order by p.lastname;
 
-end $$
+END $$
 DELIMITER ;
 
-call sp_test_math_la();
+CALL sp_test_math_la();

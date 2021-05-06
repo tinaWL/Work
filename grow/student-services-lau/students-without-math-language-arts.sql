40ee905fa22d81ead1603ce776e8fb39ce0c4ee8
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-06 19:01:07 -0400

    students without 504/IEP

--- grow/student-services-lau/students-without-math-language-arts.sql
+++ grow/student-services-lau/students-without-math-language-arts.sql
@@ -4,117 +4,97 @@ CREATE PROCEDURE sp_test_math_la()
 
 BEGIN
 
-drop temporary table if exists mla_all;
-create temporary table mla_all 
+DROP TEMPORARY TABLE IF EXISTS mla_all;
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_all 
 
-SELECT distinct  pr.programid as 'prid', e.personid as 'pid', cf.filecount as 'LP'
-    FROM person p 
-    JOIN enrollment e USING (personid)
-    JOIN `user` u ON u.userid=e.personid 
+SELECT DISTINCT  e.programid AS 'prid', e.personid AS 'pid'
+    FROM enrollment e 
     JOIN section s USING (sectionid)
-    JOIN class c ON c.classid=s.classid
-    JOIN semester m USING (semesterid)
-    JOIN `schedule` h ON h.sectionid=s.sectionid 
-    JOIN person t ON t.personid=s.teacherid 
-    JOIN program_class pc ON pc.classid=s.classid
-    JOIN program pr ON pr.programid=pc.programid --  and pr.programid IN(5,2,18,20,21,11)
     JOIN enrollmenttostudentdebit esd USING (enrollmentid)
     JOIN studentdebit sd USING (studentdebitid)
     LEFT JOIN thirdpartypayerengagement tppe USING (tppengagementid)
     LEFT JOIN thirdpartypayer tpp ON tpp.TPPayerID=tppe.TPPayerID
-     LEFT JOIN ( -- 504/IEP info
-        SELECT pf.personid, COUNT(pf.person_fileid) AS 'FileCount'
-        FROM person_file pf 
-        WHERE pf.is_confidential=1
-        GROUP BY pf.personid) AS cf ON cf.personid=p.personid
 
-WHERE s.semesterid = 44 AND e.credithours > 0 AND e.statusid = 1 AND h.begindate < DATE(NOW()) AND tpp.TPPayerID = 12612
+WHERE s.semesterid >= 46 AND e.credithours > 0 AND e.statusid = 1  AND tpp.TPPayerID = 12612 
 order by pid;
         
 
-drop temporary table if exists mla_both; -- all students currently enrolled in math and/or language arts classes
-create temporary table mla_both
-SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
+DROP TEMPORARY TABLE IF EXISTS mla_both; -- all students currently enrolled in math and/or language arts classes
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_both
+SELECT DISTINCT  ma.prid AS 'prid', ma.pid AS 'pid'
     FROM mla_all ma 
-    where ma.prid in(2,5,18,20)
+    WHERE ma.prid in(2,5,18,20)
+ORDER BY pid;
+
+
+DROP TEMPORARY TABLE IF EXISTS mla_no_math; -- students not enrolled in any math class
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_no_math
+SELECT distinct  ma.prid AS 'prid', ma.pid AS 'pid'
+    FROM mla_all ma 
+    WHERE ma.pid NOT IN(SELECT pid FROM mla_both WHERE prid IN(2,18))
+ORDER BY pid;
+
+DROP TEMPORARY TABLE IF EXISTS mla_no_la; -- students not enrolled in any language arts class
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_no_la
+SELECT distinct  ma.prid AS 'prid', ma.pid AS 'pid'
+    FROM mla_all ma 
+    WHERE ma.pid NOT IN(SELECT pid FROM mla_both WHERE prid IN(5,20))
 order by pid;
 
-
-drop temporary table if exists mla_no_math; -- students not enrolled in any math class
-create temporary table mla_no_math
-SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
+DROP TEMPORARY TABLE IF EXISTS mla_neither; -- students enrolled in NEITHER math nor language arts 
+CREATE TEMPORARY TABLE IF NOT EXISTS mla_neither
+SELECT DISTINCT ma.pid AS 'pid'
     FROM mla_all ma 
-    where ma.pid not in(select pid from mla_both where prid in(2,18))
-order by pid;
-
-drop temporary table if exists mla_no_la; -- students not enrolled in any language arts class
-create temporary table mla_no_la
-SELECT distinct  ma.prid as 'prid', ma.pid as 'pid'
-    FROM mla_all ma 
-    where ma.pid not in(select pid from mla_both where prid in(5,20))
-order by pid;
-
-drop temporary table if exists mla_neither; -- students enrolled in NEITHER math nor language arts 
-create temporary table mla_neither
-SELECT distinct ma.pid as 'pid'
-    FROM mla_all ma 
-    where ma.pid not in(select pid from mla_both) 
-    order by pid;
+    WHERE ma.pid NOT IN(SELECT pid FROM mla_both) 
+ORDER BY pid;
     
 
-drop temporary table if exists math_final; -- students ONLY missing math
-create temporary table math_final
-SELECT distinct  pid
+DROP TEMPORARY TABLE IF EXISTS math_final; -- students ONLY missing math
+CREATE TEMPORARY TABLE IF NOT EXISTS math_final
+SELECT DISTINCT pid
     FROM mla_no_math 
-    where pid not in(select pid from mla_neither) 
+    WHERE pid NOT IN(SELECT pid FROM mla_neither) 
     order by pid;
     
     
-drop temporary table if exists la_final; -- students ONLY missing language arts
-create temporary table la_final
-SELECT distinct  pid
+DROP TEMPORARY TABLE IF EXISTS la_final; -- students ONLY missing language arts
+CREATE TEMPORARY TABLE IF NOT EXISTS la_final
+SELECT DISTINCT  pid
     FROM mla_no_la 
-    where pid not in(select pid from mla_neither) 
-    order by pid;
+    WHERE pid NOT IN(SELECT pid FROM mla_neither) 
+ORDER BY pid;
 
 
-drop temporary table if exists final; -- combination of students ONLY missing math, ONLY missing language arts, and missing NEITHER (no overlap)
-create temporary table final
-select * from la_final
-group by la_final.pid
+DROP TEMPORARY TABLE IF EXISTS final; -- combination of students ONLY missing math, ONLY missing language arts, and missing NEITHER (no overlap)
+CREATE TEMPORARY TABLE IF NOT EXISTS final
+SELECT *, 'Language Arts' AS 'Missing' FROM la_final
+GROUP BY la_final.pid
 
 UNION
 
-select * from math_final
+SELECT *, 'Math' AS 'Missing' FROM math_final
 group by math_final.pid
 
 UNION
 
-select * from mla_neither
-group by mla_neither.pid;
+SELECT *, 'Math or Language Arts' AS 'Missing' FROM mla_neither 
+GROUP BY mla_neither.pid;
 
-select f.pid as 'student id', ma.lp as '504/IEP' from mla_all ma
-left join final f on ma.pid = f.pid
-group by f.pid;
+	
 
-/*drop temporary table if exists mla_none;
-create temporary table mla_none
-SELECT distinct ma.pid as 'pid', ma.prid as 'prid'
-    FROM mla_neither ma
-    where ma.pid not in(select pid from mla_no_math) 
-    order by pid;*/
+-- INSERT INTO externalactivity(`RecipientPersonID`, `ActivityType`, `ExternalID`, `CustomField01`, `CustomField02`,`CreatedDate`)
+SELECT DISTINCT f.pid-- , 'InfusionSoft_Email', 13312, p.firstname, f.missing, NOW()
+FROM final f
+JOIN person p on p.personid = f.pid
+LEFT JOIN ( -- 504/IEP info
+        SELECT pf.personid, COUNT(pf.person_fileid) AS 'FileCount'
+        FROM person_file pf 
+        WHERE pf.is_confidential=1
+        GROUP BY pf.personid) AS cf ON cf.personid=p.personid
+WHERE cf.FileCount IS NULL
+order by p.lastname;
 
-    
--- select * from mla_all where pid not in (select pid from mla_both) -- don't have either 
--- select * from mla_all where pid not in(select pid from mla_both where prid in(2,18)) -- don't have math
--- select * from mla_all where pid not in(select pid from mla_both where prid in(5,20)) -- don't have language arts
-/*select ma.pid, la.pid, nm.pid, n.pid from mla_all ma
-left join mla_no_la la on la.pid = ma.pid
-left join mla_no_math nm on nm.pid = la.pid
-left join mla_neither n on n.pid = nm.pid
-group by ma.pid;*/
--- left join mla_no_math nm on nm.pid = la.pid
-end $$
+END $$
 DELIMITER ;
 
-call sp_test_math_la();
+CALL sp_test_math_la();

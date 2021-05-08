fb1a3acff6332c6430c50f5fca539f1407310e6f
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-08 00:36:23 -0400

    cleaned up code

--- back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
+++ back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
@@ -3,7 +3,10 @@ FROM person p
 JOIN enrollment e USING (personid)        
 JOIN section s USING (sectionid)
 JOIN semester m USING (semesterid)
-WHERE s.classid in(215, 251,439,711,784) AND e.statusid =1 AND s.semesterid=46 
+JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
+JOIN studentdebit sd USING (studentdebitid)    
+JOIN thirdpartypayerengagement tppe USING (tppengagementid)   
+WHERE s.classid in(215, 251,439,711,784) AND e.statusid =1 AND s.semesterid=46 and tppe.TPPayerID = 12612
 
 UNION
 
@@ -13,20 +16,11 @@ UNION
         join person p on p.personid = eee.personid
         JOIN section see USING (sectionid)
         JOIN semester mee ON mee.semesterid=see.semesterid
-        JOIN `schedule` dee ON dee.sectionid=see.sectiongroupid 
+        JOIN `schedule` dee ON dee.sectionid=see.sectionid 
         JOIN class cee ON cee.classid=see.classid
-        WHERE dee.scheduledesc LIKE "%LAU Funded%" AND eee.statusid =1 AND mee.semesterid=46  
+        WHERE dee.scheduledesc LIKE "%LAU Funded%" AND eee.statusid =1 AND mee.semesterid=46 AND see.sectionid NOT IN(36515,36517,36519)
         
         
-        UNION
-        
-    select e.personid, p.lastname, p.firstname
-FROM person p        
-JOIN enrollment e USING (personid)        
-JOIN section s USING (sectionid)
-JOIN semester m USING (semesterid)
-WHERE s.classid in(272) AND e.statusid =1 AND s.semesterid=46   and s.sectionid = 36663  
-        
- 
+
 
 ORDER BY `lastname` ASC

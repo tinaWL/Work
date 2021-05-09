0650131a8ddfa32190a9c2ee4714c37b0224cbbd
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-09 15:07:13 -0400

    made semester aware

--- back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
+++ back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
@@ -4,6 +4,47 @@ CREATE PROCEDURE pepve()
 
 BEGIN
 
+
+/*
+* Semester / AY-aware logic
+* F/W SemesterIDs and abbreviations for current AY are available as variables 
+* Switches to upcoming AY (F/W) on Summer semester
+*/
+DECLARE _FallSemesterID           INT;  
+DECLARE _WinterSemesterID           INT;  
+DECLARE _CurrentSemesterID          INT;  
+DECLARE _CurrentSemesterName VARCHAR(3);  
+DECLARE _WinterSemesterAbbrv VARCHAR(3);
+DECLARE _FallSemesterAbbrv VARCHAR(3);
+DECLARE _SemName VARCHAR(3);
+
+-- get current SemesterID, or next SemesterID when between semesters
+SET _CurrentSemesterID   = (SELECT MAX(semesterid) FROM semester 
+                            WHERE NOW() BETWEEN DATE_SUB(begindate, INTERVAL 30 DAY) -- don't switch 'target' semesters until the current semester is well underway. 
+                            AND DATE_ADD(enddate, INTERVAL 30 DAY));                 -- handle 'between semesters' scenarios
+
+SET _CurrentSemesterName = (SELECT LEFT(semestername, 1) FROM semester WHERE semesterid = _CurrentSemesterID);
+
+SET _FallSemesterID =
+	CASE
+    	WHEN _CurrentSemesterName = 'S' THEN _CurrentSemesterID + 1
+        WHEN _CurrentSemesterName = 'F' THEN _CurrentSemesterID
+        ELSE _CurrentSemesterID - 1 -- don't update until the next AY
+        END;
+        
+SET _WinterSemesterID =
+	CASE
+    	WHEN _CurrentSemesterName = 'S' THEN _CurrentSemesterID + 2
+        WHEN _CurrentSemesterName = 'F' THEN _CurrentSemesterID + 1
+        ELSE _CurrentSemesterID
+        END;
+    	
+SET _WinterSemesterAbbrv = (SELECT CONCAT(RIGHT(semestername, 2), LEFT(semestername, 1)) FROM semester WHERE semesterid = _WinterSemesterID);
+SET _FallSemesterAbbrv = (SELECT CONCAT(RIGHT(semestername, 2), LEFT(semestername, 1)) FROM semester WHERE semesterid = _FallSemesterID);
+-- for testing purposes only, to verify variables are calculated correctly at different times of the year. Uncomment next row and comment out the entire SELECT beneath it.
+-- SELECT _CurrentSemesterID, _CurrentSemesterName, _WinterSemesterID, _FallSemesterID, _WinterSemesterAbbrv, _FallSemesterAbbrv;
+
+
 /*
 * All LAU students enrolled in ILC elective blocks, OA, Debate, and LAU Funded LEMI
 *
@@ -38,7 +79,7 @@ SELECT  e.personid AS 'id', p.lastname AS 'lname', p.firstname AS 'fname', s.cla
 	JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
 	JOIN studentdebit sd USING (studentdebitid)    
 	JOIN thirdpartypayerengagement tppe USING (tppengagementid)
-WHERE s.classid in(251,439,711,784,868,203,204,205,206) AND e.statusid =1 AND s.semesterid in(46,47) and tppe.TPPayerID = 12612
+WHERE s.classid in(251,439,711,784,868,203,204,205,206) AND e.statusid =1 AND s.semesterid in(_FallSemesterID, _WinterSemesterID) and tppe.TPPayerID = 12612
 
 UNION
 
@@ -46,7 +87,7 @@ UNION
 * LAU Students enrolled in any "LAU Funded..." courses
 * Section IDs 36515,36517,3651 are all WL IS Electives
 */
-SELECT e.personid AS 'id', p.lastname AS 'lname', p.firstname AS 'fname',  s.classid AS 'classid', dee.scheduledesc AS 'class', m.semesterid as 'sid'
+SELECT e.personid AS 'id', p.lastname AS 'lname', p.firstname AS 'fname',  s.classid AS 'classid', dee.scheduledesc AS 'class', m.semesterid AS 'sid'
 	FROM enrollment e
 	JOIN person p on p.personid = e.personid
 	JOIN section s USING (sectionid)
@@ -56,7 +97,7 @@ SELECT e.personid AS 'id', p.lastname AS 'lname', p.firstname AS 'fname',  s.cla
 	JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
 	JOIN studentdebit sd USING (studentdebitid)    
 	JOIN thirdpartypayerengagement tppe USING (tppengagementid)
-WHERE dee.scheduledesc LIKE "%LAU Funded%" AND e.statusid = 1 AND m.semesterid IN(46,47) AND s.sectionid NOT IN(36515,36517,36519) AND tppe.TPPayerID = 12612;
+WHERE dee.scheduledesc LIKE "%LAU Funded%" AND e.statusid = 1 AND m.semesterid IN(_FallSemesterID,_WinterSemesterID) AND s.sectionid NOT IN(36515,36517,36519) AND tppe.TPPayerID = 12612;
 
 
 /*
@@ -66,7 +107,7 @@ WHERE dee.scheduledesc LIKE "%LAU Funded%" AND e.statusid = 1 AND m.semesterid I
 */
 DROP TEMPORARY TABLE IF EXISTS _v_all; 
 CREATE TEMPORARY TABLE IF NOT EXISTS _v_all
-SELECT id AS 'id', lname AS 'lname', fname AS 'fname', class AS 'class' ,  IF(class='ILC BLOCK',2,1) AS 'v', sid as 'sid'
+SELECT id AS 'id', lname AS 'lname', fname AS 'fname', class AS 'class' ,  IF(class='ILC BLOCK',2,1) AS 'v', sid AS 'sid'
 	FROM _all
  GROUP BY id, class, sid
 ORDER BY id;
@@ -77,11 +118,11 @@ ORDER BY id;
 * All students who are taking more than 3 out 
 * of the 5 permitted electives in a given semester
 */
-SELECT id, lname, fname, class, SUM(v), sid
+SELECT id, lname, fname, class, SUM(v), sid, IF(sid = _FallSemesterID, _FallSemesterAbbrv, _WinterSemesterAbbrv) AS 'SemAbbrv'
 	FROM _v_all
 GROUP BY id,sid
 HAVING SUM(v) > 3
-order by sid, id;
+ORDER BY sid, id;
 
 
 END$$

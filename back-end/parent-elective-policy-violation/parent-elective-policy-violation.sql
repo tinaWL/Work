f39c1a0bfc4379e90e700f6ad2f54b682b966b8a
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-08 22:36:41 -0400

    Update parent-elective-policy-violation.sql

--- back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
+++ back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
@@ -1,3 +1,8 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS pepve$$
+CREATE PROCEDURE pepve()
+
+BEGIN
 /*
 * All LAU students enrolled in ILC elective blocks, OA, Debate, and LAU Funded LEMI
 *
@@ -12,57 +17,9 @@
 * 784 - MS Debate
 * 868 - LAU Funded LEMI
 */
-select e.personid, p.lastname, p.firstname,
-(SELECT CASE
-	WHEN s.classid IN(203,204,205,206) THEN 'ILC BLOCK'
-    WHEN s.classid = 251 THEN 'Debate I'
-    WHEN s.classid = 439 THEN 'Debate II'
-    WHEN s.classid = 711 THEN 'OA'
-    WHEN s.classid = 784 THEN 'MS Debate'
-    WHEN s.classid = 868 THEN 'LEMI'
-    ELSE '????????'
-    END) AS 'class'
-FROM person p    
-JOIN enrollment e USING (personid)        
-JOIN section s USING (sectionid)
-Join schedule h on h.sectionid = s.sectionid
-JOIN semester m USING (semesterid)
-JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
-JOIN studentdebit sd USING (studentdebitid)    
-JOIN thirdpartypayerengagement tppe USING (tppengagementid)
-WHERE s.classid in(251,439,711,784,868,203,204,205,206) AND e.statusid =1 AND s.semesterid IN(46,47) and tppe.TPPayerID = 12612
-
-
--- group by p.personid
-
-UNION all 
-
-
-/*
-* LAU Students enrolled in any "LAU Funded..." courses
-* Section IDs 36515,36517,3651 are all WL IS Electives
-*/
-SELECT eee.personid, p.lastname, p.firstname, dee.scheduledesc
-FROM enrollment eee 
-join person p on p.personid = eee.personid
-JOIN section see USING (sectionid)
-JOIN semester mee ON mee.semesterid=see.semesterid
-JOIN `schedule` dee ON dee.sectionid=see.sectionid 
-JOIN class cee ON cee.classid=see.classid
-JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
-JOIN studentdebit sd USING (studentdebitid)    
-JOIN thirdpartypayerengagement tppe USING (tppengagementid)
-WHERE dee.scheduledesc LIKE "%LAU Funded%" AND eee.statusid =1 AND mee.semesterid IN(46,47) AND see.sectionid NOT IN(36515,36517,36519) and tppe.TPPayerID = 12612
-
-
-ORDER BY `lastname` ASC
-
---------
-
---- ALL LAU STUDENTS ENROLLED IN ILC ELECTIVE BLOCKS
-DROP TEMPORARY TABLE IF EXISTS LAU_ILC;
-CREATE TEMPORARY TABLE IF NOT EXISTS LAU_ILC
-SELECT e.personid, p.lastname, p.firstname, 
+DROP TEMPORARY TABLE IF EXISTS _all; -- ALL LAU students enrolled in ILC elective blocks, OA, Debate, or LAU Funded courses
+CREATE TEMPORARY TABLE IF NOT EXISTS _all
+select  e.personid as 'id', p.lastname as 'lname', p.firstname as 'fname', s.classid as 'classid',
 (SELECT CASE
 	WHEN s.classid IN(203,204,205,206) THEN 'ILC Block'
     WHEN s.classid = 251 THEN 'Debate I'
@@ -70,7 +27,7 @@ SELECT e.personid, p.lastname, p.firstname,
     WHEN s.classid = 711 THEN 'OA'
     WHEN s.classid = 784 THEN 'MS Debate'
     WHEN s.classid = 868 THEN 'LEMI'
-    ELSE '????????'
+    ELSE '????????' -- testing
     END) AS 'class'
 FROM person p    
 JOIN enrollment e USING (personid)        
@@ -80,5 +37,48 @@ JOIN semester m USING (semesterid)
 JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
 JOIN studentdebit sd USING (studentdebitid)    
 JOIN thirdpartypayerengagement tppe USING (tppengagementid)
-WHERE s.classid in(203,204,205,206) AND e.statusid =1 AND s.semesterid IN(46,47) and tppe.TPPayerID = 12612  
-GROUP BY e.personid;
+WHERE s.classid in(251,439,711,784,868,203,204,205,206) AND e.statusid =1 AND s.semesterid =46 and tppe.TPPayerID = 12612
+
+UNION
+
+/*
+* LAU Students enrolled in any "LAU Funded..." courses
+* Section IDs 36515,36517,3651 are all WL IS Electives
+*/
+SELECT e.personid as 'id', p.lastname as 'lname', p.firstname as 'fname',  s.classid as 'classid', dee.scheduledesc as 'class'
+FROM enrollment e
+join person p on p.personid = e.personid
+JOIN section s USING (sectionid)
+JOIN semester m ON m.semesterid=s.semesterid
+JOIN `schedule` dee ON dee.sectionid=s.sectionid 
+JOIN class c ON c.classid=s.classid
+JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
+JOIN studentdebit sd USING (studentdebitid)    
+JOIN thirdpartypayerengagement tppe USING (tppengagementid)
+WHERE dee.scheduledesc LIKE "%LAU Funded%" AND e.statusid =1 AND m.semesterid =46 AND s.sectionid NOT IN(36515,36517,36519) and tppe.TPPayerID = 12612;
+
+
+DROP TEMPORARY TABLE IF EXISTS _ilc; -- LAU students enrolled in ILC elective blocks
+CREATE TEMPORARY TABLE IF NOT EXISTS _ilc
+SELECT id as 'id', lname as 'lname', fname as 'fname', class as 'class', classid as 'classid'
+	FROM _all 
+WHERE classid IN(203,204,205,206);
+
+
+/*
+* ILC students with an elective policy violation 
+* (ILC blocks count x2, so this calculation is done separately)
+*/
+SELECT i.id, i.lname, i.fname, a.class, COUNT(DISTINCT i.id, a.class)
+	FROM _ilc i
+	JOIN _all a ON a.id = i.id
+    WHERE a.classid NOT IN(203,204,205,206) -- don't count enrollments in ILC blocks
+GROUP BY i.id
+HAVING COUNT(DISTINCT i.id, a.class) > 1 -- students are allowed 1 additional elective 
+ORDER BY i.id;
+
+
+
+END$$
+DELIMITER ;
+CALL pepve();

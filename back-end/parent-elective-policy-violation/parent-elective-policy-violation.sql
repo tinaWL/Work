30a4771e2935232ac161c92438e11fd993c9e03c
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-08 23:59:02 -0400

    Update parent-elective-policy-violation.sql

--- back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
+++ back-end/parent-elective-policy-violation/parent-elective-policy-violation.sql
@@ -3,6 +3,7 @@ DROP PROCEDURE IF EXISTS pepve$$
 CREATE PROCEDURE pepve()
 
 BEGIN
+
 /*
 * All LAU students enrolled in ILC elective blocks, OA, Debate, and LAU Funded LEMI
 *
@@ -19,24 +20,24 @@ BEGIN
 */
 DROP TEMPORARY TABLE IF EXISTS _all; -- ALL LAU students enrolled in ILC elective blocks, OA, Debate, or LAU Funded courses
 CREATE TEMPORARY TABLE IF NOT EXISTS _all
-select  e.personid as 'id', p.lastname as 'lname', p.firstname as 'fname', s.classid as 'classid',
+SELECT  e.personid AS 'id', p.lastname AS 'lname', p.firstname AS 'fname', s.classid AS 'classid',
 (SELECT CASE
-	WHEN s.classid IN(203,204,205,206) THEN 'ILC Block'
-    WHEN s.classid = 251 THEN 'Debate I'
-    WHEN s.classid = 439 THEN 'Debate II'
-    WHEN s.classid = 711 THEN 'OA'
-    WHEN s.classid = 784 THEN 'MS Debate'
-    WHEN s.classid = 868 THEN 'LEMI'
-    ELSE '????????' -- testing
-    END) AS 'class'
-FROM person p    
-JOIN enrollment e USING (personid)        
-JOIN section s USING (sectionid)
-Join schedule h on h.sectionid = s.sectionid
-JOIN semester m USING (semesterid)
-JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
-JOIN studentdebit sd USING (studentdebitid)    
-JOIN thirdpartypayerengagement tppe USING (tppengagementid)
+	    WHEN s.classid IN(203,204,205,206) THEN 'ILC Block'
+	    WHEN s.classid = 251 THEN 'Debate I'
+	    WHEN s.classid = 439 THEN 'Debate II'
+	    WHEN s.classid = 711 THEN 'OA'
+	    WHEN s.classid = 784 THEN 'MS Debate'
+	    WHEN s.classid = 868 THEN 'LEMI'
+	    ELSE '????????' -- testing
+        END) AS 'class'
+	FROM person p    
+	JOIN enrollment e USING (personid)        
+	JOIN section s USING (sectionid)
+	Join schedule h on h.sectionid = s.sectionid
+	JOIN semester m USING (semesterid)
+	JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
+	JOIN studentdebit sd USING (studentdebitid)    
+	JOIN thirdpartypayerengagement tppe USING (tppengagementid)
 WHERE s.classid in(251,439,711,784,868,203,204,205,206) AND e.statusid =1 AND s.semesterid =46 and tppe.TPPayerID = 12612
 
 UNION
@@ -45,17 +46,17 @@ UNION
 * LAU Students enrolled in any "LAU Funded..." courses
 * Section IDs 36515,36517,3651 are all WL IS Electives
 */
-SELECT e.personid as 'id', p.lastname as 'lname', p.firstname as 'fname',  s.classid as 'classid', dee.scheduledesc as 'class'
-FROM enrollment e
-join person p on p.personid = e.personid
-JOIN section s USING (sectionid)
-JOIN semester m ON m.semesterid=s.semesterid
-JOIN `schedule` dee ON dee.sectionid=s.sectionid 
-JOIN class c ON c.classid=s.classid
-JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
-JOIN studentdebit sd USING (studentdebitid)    
-JOIN thirdpartypayerengagement tppe USING (tppengagementid)
-WHERE dee.scheduledesc LIKE "%LAU Funded%" AND e.statusid =1 AND m.semesterid =46 AND s.sectionid NOT IN(36515,36517,36519) and tppe.TPPayerID = 12612;
+SELECT e.personid AS 'id', p.lastname AS 'lname', p.firstname AS 'fname',  s.classid AS 'classid', dee.scheduledesc AS 'class'
+	FROM enrollment e
+	JOIN person p on p.personid = e.personid
+	JOIN section s USING (sectionid)
+	JOIN semester m ON m.semesterid=s.semesterid
+	JOIN `schedule` dee ON dee.sectionid=s.sectionid 
+	JOIN class c ON c.classid=s.classid
+	JOIN enrollmenttostudentdebit esd USING (enrollmentid)    
+	JOIN studentdebit sd USING (studentdebitid)    
+	JOIN thirdpartypayerengagement tppe USING (tppengagementid)
+WHERE dee.scheduledesc LIKE "%LAU Funded%" AND e.statusid = 1 AND m.semesterid =46 AND s.sectionid NOT IN(36515,36517,36519) AND tppe.TPPayerID = 12612;
 
 
 /*
@@ -65,10 +66,10 @@ WHERE dee.scheduledesc LIKE "%LAU Funded%" AND e.statusid =1 AND m.semesterid =4
 */
 DROP TEMPORARY TABLE IF EXISTS _v_all; 
 CREATE TEMPORARY TABLE IF NOT EXISTS _v_all
-SELECT id as 'id', lname as 'lname', fname as 'fname', class as 'class' ,  if(class='ILC BLOCK',2,1) as 'v'
-from _all
-group by id, class
-order by id;
+SELECT id AS 'id', lname AS 'lname', fname AS 'fname', class AS 'class' ,  IF(class='ILC BLOCK',2,1) AS 'v'
+	FROM _all
+GROUP BY id, class
+ORDER BY id;
 
 
 /*

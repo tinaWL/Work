59577829f207cb66da534ce6e1bea9714a4535c8
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-12 12:57:26 -0400

    updated to use sem abbreviations

--- grow/coo/partner-school-info.sql
+++ grow/coo/partner-school-info.sql
@@ -1,8 +1,9 @@
 -- https://app.asana.com/0/1200281177330435/1200281177330460/f
 -- CREATE OR REPLACE VIEW `sis_live`.`vw_partner_school_info` AS
-SELECT tpp.tppayerid, tppp.preferredname AS 'Third-Party Payer', tppp.state AS 'State', 
+SELECT tpp.tppayerid, tppp.preferredname AS 'Third-Party Payer', 
+tppp.state AS 'State', 
 (CASE WHEN tpp.tppapproved=1 THEN 'Active' ELSE 'Not Active' END) AS 'Status', 
-tppfs.Semester AS 'First Semester',
+CONCAT(RIGHT(tppfs.Semester, 2), LEFT(tppfs.Semester, 1)) AS 'First Semester',
 -- If tpp is active, display student count, otherwise display last active semester
 IF(tpp.TPPApproved=1, 
 (SELECT COUNT(DISTINCT(e.personid))
@@ -16,12 +17,13 @@ IF(tpp.TPPApproved=1,
  		NOT IN (414,497,502,543,544,545,546,502,551) 
  		AND asd.studentdebitid IS NULL 
  		AND e.statusid IN (1,4) 
-        AND tpp.tppayerid=tppe.tppayerid), tppfs.max_s) AS 'Last Semester'
+        AND tpp.tppayerid=tppe.tppayerid), 
+ CONCAT(RIGHT(tppfs.max_s, 2), LEFT(tppfs.max_s, 1))) AS 'Last Semester'
 
 FROM thirdpartypayer tpp   
 JOIN person tppp ON tpp.tppayerpersonid=tppp.personid 
 LEFT JOIN (
-    SELECT tppe.tppayerid, MIN(m.semesterid), m.semestername AS 'Semester', MAX(m.semesterid) as 'Max_n', MAX(m.semestername) as 'Max_s'
+    SELECT tppe.tppayerid, MIN(m.semesterid), m.semestername AS 'Semester', MAX(m.semestername) as 'Max_s'
     FROM enrollment e
     JOIN section s USING (sectionid)
     JOIN semester m USING (semesterid)
@@ -32,4 +34,4 @@ LEFT JOIN (
     WHERE s.classid NOT IN (414,497,502,543,544,545,546,502,551) AND asd.studentdebitid IS NULL AND e.statusid IN (1,4) 
        AND sd.obsolete=0
     GROUP BY tppe.tppayerid) AS tppfs ON tppfs.tppayerid=tpp.tppayerid  
-ORDER BY `First Semester` ASC
+ORDER BY `First Semester`;

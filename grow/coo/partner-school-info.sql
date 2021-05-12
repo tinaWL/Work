d9f029fe6966a71e95bc207f8b2098effd65c739
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-12 13:19:52 -0400

    make SP

--- grow/coo/partner-school-info.sql
+++ grow/coo/partner-school-info.sql
@@ -1,5 +1,25 @@
 -- https://app.asana.com/0/1200281177330435/1200281177330460/f
--- CREATE OR REPLACE VIEW `sis_live`.`vw_partner_school_info` AS
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `sp_partner_schools_info` $$
+CREATE PROCEDURE `sp_partner_schools_info`()
+BEGIN
+
+-- Info to grab info for current semester, skipping Summer
+
+
+DECLARE _TargetSemesterID           INT;  -- Semeter to measure 
+DECLARE _CurrentSemesterID          INT;  -- current semester to get a baseline
+DECLARE _CurrentSemesterName VARCHAR(3);  -- determine whether current semester is Fall, Winter, or Summer
+
+-- get current SemesterID, or next SemesterID when between semesters
+SET _CurrentSemesterID   = (SELECT MAX(semesterid) FROM semester 
+                            WHERE NOW() BETWEEN DATE_SUB(begindate, INTERVAL 30 DAY) -- don't switch 'target' semesters until the current semester is well underway. 
+                            AND DATE_ADD(enddate, INTERVAL 30 DAY));                 -- handle 'between semesters' scenarios
+
+SET _CurrentSemesterName = (SELECT LEFT(semestername, 1) FROM semester WHERE semesterid = _CurrentSemesterID);
+SET _TargetSemesterID = IF(_CurrentSemesterName = 'S', _CurrentSemesterID + 1, _CurrentSemesterID); -- skip Summer semester
+
 SELECT tpp.tppayerid, tppp.preferredname AS 'Third-Party Payer', 
 tppp.state AS 'State', 
 (CASE WHEN tpp.tppapproved=1 THEN 'Active' ELSE 'Not Active' END) AS 'Status', 
@@ -8,7 +28,7 @@ CONCAT(RIGHT(tppfs.Semester, 2), LEFT(tppfs.Semester, 1)) AS 'First Semester',
 IF(tpp.TPPApproved=1, 
 (SELECT COUNT(DISTINCT(e.personid))
      FROM enrollment e
-     JOIN section s USING (sectionid)
+     JOIN section s ON s.sectionid = e.sectionid AND s.semesterid =  _TargetSemesterID
      JOIN enrollmenttostudentdebit esd USING (enrollmentid)
      JOIN studentdebit sd USING (studentdebitid)
      JOIN thirdpartypayerengagement tppe USING (tppengagementid)
@@ -35,4 +55,10 @@ LEFT JOIN (
     WHERE s.classid NOT IN (414,497,502,543,544,545,546,502,551) AND asd.studentdebitid IS NULL AND e.statusid IN (1,4) 
        AND sd.obsolete=0
     GROUP BY tppe.tppayerid) AS tppfs ON tppfs.tppayerid=tpp.tppayerid  
-ORDER BY `First Semester`;
+ORDER BY `Third-Party Payer`;
+
+END $$
+
+DELIMITER ;
+
+CALL sp_partner_schools_info();

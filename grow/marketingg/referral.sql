9c9cdabfa55e6798a0748c717877fa1c84770df9
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-12 20:42:09 -0400

    Update referral.sql

--- grow/marketingg/referral.sql
+++ grow/marketingg/referral.sql
@@ -5,18 +5,24 @@ SELECT DISTINCT
     CONCAT(pg.lastname, ', ', pg.firstname) AS 'Parent Name',
     pg.personid AS 'Parent ID',
     pr.relationtype,
-    COUNT(DISTINCT p.personid) AS 'Children'
+    COUNT(DISTINCT p.personid) AS 'Children',
+    COUNT(e.enrollmentid) as 'Enrollments',
+    COUNT(dr.EnrollmentID) as 'Drops'
 FROM person p
 	JOIN person_relation pr on pr.secondpersonid = p.personid
     JOIN person pg on pg.personid = pr.firstpersonid
 	JOIN student_program sp ON sp.personid=p.personid AND sp.statusid=1 AND sp.is_deleted=0
 	JOIN enrollment e ON e.personid=p.personid
-    JOIN section s ON s.sectionid = e.sectionid -- AND s.semesterid = 46
-	JOIN enrollmenttostudentdebit esd USING (enrollmentid)
+    left JOIN droprequest dr on dr.EnrollmentID = e.enrollmentid
+    JOIN section s ON s.sectionid = e.sectionid 
+    JOIN semester m on m.semesterid = s.semesterid
+	JOIN enrollmenttostudentdebit esd ON esd.EnrollmentID = e.enrollmentid
 	JOIN studentdebit sd USING (studentdebitid)
 	LEFT JOIN studentdebit asd ON asd.adjustsdebitid=sd.studentdebitid AND asd.obsolete=0
 	LEFT JOIN thirdpartypayerengagement tppe ON tppe.tppengagementid=sd.tppengagementid
 	LEFT JOIN thirdpartypayer tpp ON tpp.tppayerid=tppe.tppayerid
 	LEFT JOIN person tppp ON tppp.personid=tpp.tppayerpersonid
-GROUP BY pg.personid
-ORDER BY `Student ID` ASC
+    
+--  WHERE dr.RequestDate < DATE_ADD(m.begindate, interval 28 day)
+GROUP BY pg.personid  
+HAVING COUNT(e.enrollmentid) != COUNT(dr.EnrollmentID)

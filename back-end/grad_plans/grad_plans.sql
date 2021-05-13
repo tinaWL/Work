0d7deac5726870441c111e5e18f44f104a6426e6
tinaWL <81999165+tinaWL@users.noreply.github.com>
2021-05-12 21:14:53 -0400

    Update grad_plans.sql

--- back-end/grad_plans/grad_plans.sql
+++ back-end/grad_plans/grad_plans.sql
@@ -1,43 +1,43 @@
 DELIMITER $$
-DROP PROCEDURE IF EXISTS sp_test_grad_plan$$
-CREATE PROCEDURE sp_test_grad_plan()
+DROP PROCEDURE IF EXISTS sp_grad_plan$$
+CREATE PROCEDURE sp_grad_plan()
 
 BEGIN
 
-drop temporary table if exists gpIDs;
+DROP TEMPORARY TABLE IF EXISTS gpIDs;
 
 CREATE TEMPORARY TABLE IF NOT EXISTS gpIDs AS
 
-SELECT distinct p.personid, concat(p.firstname, ' ', p.lastname) as 'name', sgy.StudentGradeYear, sp.studentprogramid, sp.programid, sp.statusid, sp.AnticipatedGradSemesterID, gp.GradPlanID
+SELECT DISTINCT p.personid, concat(p.firstname, ' ', p.lastname) AS 'name', sgy.StudentGradeYear, sp.studentprogramid, sp.programid, sp.statusid, sp.AnticipatedGradSemesterID, gp.GradPlanID
 
 FROM person p
 JOIN student_program sp ON sp.personid=p.personid AND sp.statusid=1 AND sp.is_deleted=0 AND sp.programid = 21
-LEFT JOIN gradplanperson gp on gp.StudentProgramID = sp.studentprogramid
+LEFT JOIN gradplanperson gp ON gp.StudentProgramID = sp.studentprogramid
 
 
 LEFT JOIN (
-SELECT sp.personid, MAX(sy.year_id) AS 'StudentGradeYear'
-FROM student_program sp
-JOIN program pr ON pr.programid=sp.programid
-JOIN institution_year_semester iys ON iys.semester_id >=44
-JOIN institution_year iy ON iy.id = iys.institution_year_id AND iy.institution_id=pr.institutionid
-JOIN student_year sy ON sp.studentprogramid = sy.student_program_id AND sy.institution_year_id = iy.id AND sy.is_deleted = 0
-WHERE sp.statusid IN (0,1) AND sp.is_deleted=0
-GROUP BY sp.personid) AS sgy ON sgy.personid = p.personid
+  SELECT sp.personid, MAX(sy.year_id) AS 'StudentGradeYear'
+    FROM student_program sp
+    JOIN program pr ON pr.programid=sp.programid
+    JOIN institution_year_semester iys ON iys.semester_id >=44
+    JOIN institution_year iy ON iy.id = iys.institution_year_id AND iy.institution_id=pr.institutionid
+    JOIN student_year sy ON sp.studentprogramid = sy.student_program_id AND sy.institution_year_id = iy.id AND sy.is_deleted = 0
+  WHERE sp.statusid IN (0,1) AND sp.is_deleted=0
+  GROUP BY sp.personid) AS sgy ON sgy.personid = p.personid
 
 WHERE (sgy.studentGradeYear between 8 and 12) and gp.GradPlanID is null;
 
-insert into gradplanperson(`studentprogramid`, `gradplanid`, `createdbyid`,`createddate`)
-select studentprogramid, 1, 22365, NOW()
-from gpids;
+INSERT INTO gradplanperson(`studentprogramid`, `gradplanid`, `createdbyid`,`createddate`)
+SELECT studentprogramid, 1, 22365, NOW()
+FROM gpids;
 
 
 INSERT INTO gradplanpersonhistory(`gradplanpersonid`, `date`, `personid`)
-select gp.gradplanpersonid, NOW(),22365
-from gpids
-join gradplanperson gp using (studentprogramid);
+SELECT gp.gradplanpersonid, NOW(),22365
+  FROM gpids
+  JOIN gradplanperson gp using (studentprogramid);
 
-end $$
+END $$
 DELIMITER ;
 
-call sp_test_grad_plan();
+CALL sp_grad_plan();
